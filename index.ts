6bcca9ca39e59eaf173a3294a7eeefa636be0dda
Said_Nouasria <saidnouasria23@gmail.com>
2025-02-03 18:34:57 +0100

    AI chat with no memory

---
+++ index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config"
+import { runLLM } from "./src/llm"
+
+const userMessage = process.argv[2]
+
+if (!userMessage) {
+  console.error("Please provide a message")
+  process.exit(1)
+}
+
+
+const response = await runLLM({
+  messages: [{ role: "user", content: userMessage }],
+})
+
+console.log(response.content)
